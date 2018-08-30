697b38c5dcc19fb12bedabc70f7660466863f8a1
Chacha Kairu <nickchacha@gmail.com>
2018-08-30 19:00:06 +0300

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,9 @@
+{
+	'name': 'Point of Sale Dairy Report',
+	'version': '1.0.1',
+	'category':'Point Of Dairy Sale',
+	'description':"",
+	'application': True,
+	'data':[
+		'views/pos_dairyorders_report_view.xml'],
+}
