fe8a11ccf6022b70918d1256b5eda090e31671a2
Chacha Kairu <nickchacha@gmail.com>
2018-09-01 12:19:24 +0300

    updated manifest file

--- __manifest__.py
+++ __manifest__.py
@@ -1,8 +1,14 @@
 {
 	'name': 'Point of Sale Dairy Report',
-	'version': '1.0.1',
-	'category':'Point Of Dairy Sale',
-	'description':"",
+	'version': '10.0.1.0.0',
+    'summary': """Daily Sales Report """,
+    'description': """Report to generate Sales Per day""",
+    'author': 'Chacha Kairu',
+    'company': 'Fastworld Solutions',
+    'website': 'http://www.fastworldsolutions.net',
+    'category': 'Generic Modules/Point Of Sale',
+    'depends': ['stock_account', 'barcodes', 'web_editor'],
+    'license': 'LGPL-3',
 	'application': True,
 	'data':[
 		'views/pos_dairyorders_report_view.xml'],
